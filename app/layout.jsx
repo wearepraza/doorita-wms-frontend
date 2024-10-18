327a7d14939d900a168da3a66f32bd65c1f97837
Amirhosein Qadamgahi <amirhoseinqadamgahi@gmail.com>
2024-10-18 11:50:57 +0330

    created a basic project

---
+++ app/layout.jsx
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: "مدیریت انبار دوریتا",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="fa" dir="rtl">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
