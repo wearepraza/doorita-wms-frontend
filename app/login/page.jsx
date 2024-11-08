d01a918c60259c08da7bc3415e94fa8ea487f05c
mkj-sfr <djmkj11@gmail.com>
2024-11-08 05:10:20 -0800

    added the login page

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,14 +1,22 @@
 // styles
-import styles from '@/public/styles/login.scss'
+import styles from '../../public/styles/login.module.scss'
+
+// components
+import Image from 'next/image'
+
+// assets
+import logo from '@/public/assets/images/logo.svg'
 
 export default function Login() {
     return (
-        <div className={styles.loginFrom}>
-            <form>
+        <div className={styles.loginFormContainer}>
+            <Image src={logo} width={100} height={100} />
+            <form className={`${styles.loginForm} shadow`}>
                 <label htmlFor="username">نام کاربری</label>
-                <input type="text" id="username" />
+                <input type="text" id="username" placeholder='نام کاربری خود را وارد کنید' />
                 <label htmlFor="password">رمز عبور</label>
-                <input type="password" id="password" />
+                <input type="password" id="password" placeholder='رمز عبور خود را وارد کنید' />
+                <button className='primaryButton'>ورود به حساب کاربری</button>
             </form>
         </div>
     )
