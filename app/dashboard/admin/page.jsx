a47a285032eccf2255a6c392aed7866cdeb21c6d
Danial <daniialhasanzade@gmail.com>
2024-11-13 16:07:57 +0330

    Index of Admin's dashboard CREATED:

--- app/dashboard/admin/page.jsx
+++ app/dashboard/admin/page.jsx
@@ -1,13 +1,20 @@
 // styles
 import styles from '@/public/styles/dashboard.module.scss'
 
-// components
-import Navigation from '@/components/layout/navigation/Navigation'
-
 export default function Dashboard () {
     return (
-        <div>
-            <Navigation />
+        <div className="flex flex-col gap-y-10 px-2 md:px-0">
+            <h1 className='text-center font-bold'>به پنل دریتا خوش آمدید</h1>
+            <div className='grid grid-flow-row-dense grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-8 gap-y-6'>
+                <div className={styles.cardContainer}>
+                    <h3>آمار ورودی ماه</h3>
+                    <span>331 عدد</span>
+                </div>
+                <div className={styles.cardContainer}>
+                    <h3>آمار خروجی ماه</h3>
+                    <span>331 عدد</span>
+                </div>
+            </div>
         </div>
     )
 }
