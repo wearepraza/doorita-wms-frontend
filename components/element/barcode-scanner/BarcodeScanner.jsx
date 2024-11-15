b4320dc3a95600ce017b7e5d2c47156db9763e27
Amirhosein Qadamgahi <amirhoseinqadamgahi@gmail.com>
2024-11-15 18:06:11 +0330

    updated project

--- components/element/barcode-scanner/BarcodeScanner.jsx
+++ components/element/barcode-scanner/BarcodeScanner.jsx
@@ -1,6 +1,41 @@
-import { BarcodeScanner } from 'react-barcode-scanner'
-import "react-barcode-scanner/polyfill"
+'use client'
 
-export default () => {
-  return <BarcodeScanner />
+import React, { Component } from 'react'
+import { useRouter } from 'next/navigation' // Import useRouter hook from Next.js
+import Scanner from '../scanner/Scanner'
+
+class BarcodeScanner extends Component {
+    state = {
+        results: [],
+    }
+
+    _scan = () => {
+        this.setState({ scanning: !this.state.scanning })
+    }
+
+    _onDetected = result => {
+        const { code } = result.codeResult;  // Get the scanned code
+        this.setState({ results: [result] }); // Store the result
+
+        // Redirect to the dynamic route with the scanned code
+        const router = useRouter();
+        router.push(`/${code}`); // Redirect to the dynamic route
+    }
+
+    render() {
+        return (
+            <div>
+                <span>Barcode Scanner</span>
+                <Scanner onDetected={this._onDetected} />
+                <textarea
+                    style={{ fontSize: 32, width: 320, height: 100, marginTop: 30 }}
+                    rowsMax={4}
+                    defaultValue={'No data scanned'}
+                    value={this.state.results[0] ? this.state.results[0].codeResult.code : 'No data scanned'}
+                />
+            </div>
+        )
+    }
 }
+
+export default BarcodeScanner;
