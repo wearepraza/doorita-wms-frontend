67c0a4c7304fdd3e190bdffd5a75db0a54244532
Amirhosein Qadamgahi <amirhoseinqadamgahi@gmail.com>
2024-11-23 13:58:41 +0330

    updated scanner

--- components/element/scanner/Scanner.jsx
+++ components/element/scanner/Scanner.jsx
@@ -1,71 +1,55 @@
-'use client'
+'use client';
 
-import React, { Component } from 'react'
-import Quagga from 'quagga'
+import React, { Component } from 'react';
+import Quagga from '@ericblade/quagga2';
 
 class Scanner extends Component {
-  componentDidMount() {
-    Quagga.init(
-      {
-        inputStream: {
-          type: 'LiveStream',
-          constraints: {
-            width: 400,
-            height: 320,
-            facingMode: 'environment',
-          },
-        },
-        locator: {
-            halfSample: true,
-            patchSize: "large", // x-small, small, medium, large, x-large
-            debug: {
-                showCanvas: true,
-                showPatches: false,
-                showFoundPatches: false,
-                showSkeleton: false,
-                showLabels: false,
-                showPatchLabels: false,
-                showRemainingPatchLabels: false,
-                boxFromPatches: {
-                    showTransformed: true,
-                    showTransformedBox: true,
-                    showBB: true
-              }
-            }
-        },
-        numOfWorkers: 4,
-        decoder: {
-            readers: ['code_128_reader'],
-            debug: {
-                drawBoundingBox: true,
-                showFrequency: true,
-                drawScanline: true,
-                showPattern: true
+    componentDidMount() {
+        Quagga.init(
+            {
+                inputStream: {
+                    type: 'LiveStream',
+                    constraints: {
+                        width: { ideal: 400 },
+                        height: { ideal: 320 },
+                        facingMode: 'environment',
+                    },
+                },
+                locator: {
+                    halfSample: true,
+                    patchSize: 'large',
+                },
+                numOfWorkers: 4,
+                decoder: {
+                    readers: ['code_128_reader'],
+                },
+                locate: true,
             },
-        },
-        locate: true,
-      },
-      function(err) {
-        if (err) {
-          return console.log(err)
+            (err) => {
+                if (err) {
+                    console.error('Quagga init failed:', err);
+                    return;
+                }
+                Quagga.start();
+            }
+        );
+        Quagga.onDetected(this._onDetected);
+    }
+
+    componentWillUnmount() {
+        Quagga.offDetected(this._onDetected);
+        Quagga.stop(); // Stop the stream
+    }
+
+    _onDetected = (result) => {
+        if (this.props.onDetected) {
+            this.props.onDetected(result);
         }
-        Quagga.start()
-      },
-    )
-    Quagga.onDetected(this._onDetected)
-  }
+    };
 
-  componentWillUnmount() {
-    Quagga.offDetected(this._onDetected)
-  }
-
-  _onDetected = result => {
-    this.props.onDetected(result)
-  }
-
-  render() {
-    return <div id="interactive" className="viewport"/>
+    render() {
+        return <div id="interactive" className="viewport" />;
     }
 }
 
-export default Scanner
+export default Scanner;
